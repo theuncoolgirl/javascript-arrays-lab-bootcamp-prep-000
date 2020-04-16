67c58cf42a4ab0b858b072777fd7ac88577203fa
Erin Shields <elshields92@gmail.com>
2020-04-16 16:29:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,3 +21,7 @@ function appendKitten(name) {
   var moreKittens = [...kittens, name]
   return moreKittens
 }
+function prependKitten(name) {
+  var moreKittens = [name, ...kittens]
+  return moreKittens
+}
